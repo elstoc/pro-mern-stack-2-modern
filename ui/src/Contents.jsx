ddc891d64dc625db964a5b2ce34245ad75e390c3
Chris Elston <chris.elston@blueyonder.co.uk>
2022-04-29 16:26:36 +0100

    09.02-route-parameters (working/modified)
    handling of URL params changed in latest react-router-dom, now read with
    useParams()
    
    Also change how the route is specified to use element instead of
    component

--- ui/src/Contents.jsx
+++ ui/src/Contents.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import IssueList from './IssueList.jsx';
 import IssueReport from './IssueReport.jsx';
+import IssueEdit from './IssueEdit.jsx';
 
 const NotFound = () => <h1>Page Not Found</h1>;
 
@@ -11,6 +12,7 @@ export default function Contents() {
     <Routes>
       <Route path="/issues" element={<IssueList />} />
       <Route path="/report" element={<IssueReport />} />
+      <Route path="/edit/:id" element={<IssueEdit />} />
       <Route path="/" element={<Navigate to="/issues" />} />
       <Route element={<NotFound />} />
     </Routes>
